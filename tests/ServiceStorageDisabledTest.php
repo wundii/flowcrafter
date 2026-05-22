83c0bf84fd23fca4388c781777a5c3d1958d62ae
Andreas Wunderwald <wundii@gmail.com>
2026-05-22 19:36:07 +0200

    refactor: replace `createDisabledService` with `NullStorageMock` in `ServiceStorageDisabledTest` to streamline disabled storage testing

--- tests/ServiceStorageDisabledTest.php
+++ tests/ServiceStorageDisabledTest.php
@@ -8,70 +8,69 @@ use Exception;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 use Tests\MockClass\MessageInitMock;
+use Tests\MockClass\NullStorageMock;
 use Tests\MockClass\WorkflowFailMock;
 use Tests\MockClass\WorkflowMock;
-use Wundii\Flowcrafter\Enum\SortEnum;
 use Wundii\Flowcrafter\Flow;
 use Wundii\Flowcrafter\FlowRunner;
 use Wundii\Flowcrafter\Interface\MessageReturnInterface;
 use Wundii\Flowcrafter\Schedule\ScheduleException;
-use Wundii\Flowcrafter\Storage\Service;
 
 final class ServiceStorageDisabledTest extends TestCase
 {
     public function testServiceMethodsReturnEmptyWhenDisabled(): void
     {
-        $service = $this->createDisabledService();
+        $nullStorageMock = new NullStorageMock();
 
-        $this->assertFalse($service->isServiceStorageInitialized());
-        $this->assertSame(0, $service->countFlows());
-        $this->assertSame(0, $service->countExceptions());
-        $this->assertSame(0, $service->countScheduleExceptions());
-        $this->assertSame(0, $service->countObserverExceptions());
-        $this->assertSame(0, $service->countFlowsByType('flow.test'));
-        $this->assertSame(0, $service->countFlowsBySource('TestFlow'));
-        $this->assertSame(0, $service->countFlowsBySubject('test'));
-        $this->assertSame([], iterator_to_array($service->findAllFlows()));
-        $this->assertSame([], iterator_to_array($service->findFlowsByType('flow.test')));
-        $this->assertSame([], iterator_to_array($service->findFlowsBySource('TestFlow')));
-        $this->assertSame([], iterator_to_array($service->findFlowsBySubject('test')));
-        $this->assertSame([], iterator_to_array($service->findFlowStats()));
-        $this->assertSame([], iterator_to_array($service->findFlowTypeStats()));
-        $this->assertSame([], iterator_to_array($service->findAllExceptions()));
-        $this->assertSame([], iterator_to_array($service->findExceptionStats()));
+        $this->assertFalse($nullStorageMock->isServiceStorageInitialized());
+        $this->assertSame(0, $nullStorageMock->countFlows());
+        $this->assertSame(0, $nullStorageMock->countExceptions());
+        $this->assertSame(0, $nullStorageMock->countScheduleExceptions());
+        $this->assertSame(0, $nullStorageMock->countObserverExceptions());
+        $this->assertSame(0, $nullStorageMock->countFlowsByType('flow.test'));
+        $this->assertSame(0, $nullStorageMock->countFlowsBySource('TestFlow'));
+        $this->assertSame(0, $nullStorageMock->countFlowsBySubject('test'));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findAllFlows()));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findFlowsByType('flow.test')));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findFlowsBySource('TestFlow')));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findFlowsBySubject('test')));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findFlowStats()));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findFlowTypeStats()));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findAllExceptions()));
+        $this->assertSame([], iterator_to_array($nullStorageMock->findExceptionStats()));
     }
 
     public function testAppendFlowNoOpWhenDisabled(): void
     {
-        $service = $this->createDisabledService();
+        $nullStorageMock = new NullStorageMock();
 
         $flowRunner = new FlowRunner(
             type: 'flow.workflow.v1',
             flowSource: WorkflowMock::class,
-            storage: $service,
+            storage: $nullStorageMock,
         );
         $result = $flowRunner->run(new MessageInitMock('test'));
 
         $this->assertInstanceOf(MessageReturnInterface::class, $result);
-        $this->assertSame(0, $service->countFlows());
+        $this->assertSame(0, $nullStorageMock->countFlows());
     }
 
     public function testAppendScheduleExceptionNoOpWhenDisabled(): void
     {
-        $service = $this->createDisabledService();
+        $nullStorageMock = new NullStorageMock();
 
         $scheduleException = ScheduleException::create('TestSchedule', 'test', '* * * * *', 0, 'fail', __FILE__, __LINE__, '');
-        $service->appendScheduleException($scheduleException);
+        $nullStorageMock->appendScheduleException($scheduleException);
 
-        $this->assertSame(0, $service->countScheduleExceptions());
+        $this->assertSame(0, $nullStorageMock->countScheduleExceptions());
     }
 
     public function testTruncateFlowListNoOpWhenDisabled(): void
     {
-        $service = $this->createDisabledService();
+        $nullStorageMock = new NullStorageMock();
 
-        $service->truncateFlowList();
-        $this->assertSame(0, $service->countFlows());
+        $nullStorageMock->truncateFlowList();
+        $this->assertSame(0, $nullStorageMock->countFlows());
     }
 
     public function testFlowRunnerWithoutStorageStillWorks(): void
@@ -89,12 +88,12 @@ final class ServiceStorageDisabledTest extends TestCase
         $this->assertCount(0, $flow->getFlowExceptions());
     }
 
-    public function testFlowRunnerFailWithoutStorageDoesNotCrash(): void
+    public function testFlowRunnerFailWithDisabledStorageDoesNotCrash(): void
     {
         $flowRunner = new FlowRunner(
             type: 'flow.workflow.fail.v1',
             flowSource: WorkflowFailMock::class,
-            storage: $this->createDisabledService(),
+            storage: new NullStorageMock(),
         );
 
         try {
@@ -103,119 +102,5 @@ final class ServiceStorageDisabledTest extends TestCase
         } catch (Exception $exception) {
             $this->assertInstanceOf(RuntimeException::class, $exception);
         }
-    }
-
-    private function createDisabledService(): Service
-    {
-        return new class() extends Service {
-            public function isPrimaryStorageInitialized(): bool
-            {
-                return true;
-            }
-
-            public function initializeDatabase(): void
-            {
-            }
-
-            public function registerFlowSchema(\Wundii\Flowcrafter\FlowSchema $flowSchema): void
-            {
-            }
-
-            public function registerMessageSource(\Wundii\Flowcrafter\Storage\Entity\MessageSourceEntity $messageSourceEntity): void
-            {
-            }
-
-            public function registerStepSource(\Wundii\Flowcrafter\Storage\Entity\StepSourceEntity $stepSourceEntity): void
-            {
-            }
-
-            public function registerFlowInstance(Flow $flow): void
-            {
-            }
-
-            public function appendFlowRun(Flow $flow, ?string $queueId = null): void
-            {
-            }
-
-            public function appendFlowMessage(\Wundii\Flowcrafter\FlowMessage $flowMessage): void
-            {
-            }
-
-            public function appendFlowException(\Wundii\Flowcrafter\FlowException $flowException): void
-            {
-            }
-
-            public function appendFlowResult(\Wundii\Flowcrafter\FlowResult $flowResult): void
-            {
-            }
-
-            public function appendFlowRetry(\Wundii\Flowcrafter\FlowRetry $flowRetry): void
-            {
-            }
-
-            public function appendObserveItem(string $type, string $flowSource, ?string $flowHash, string $messageSource, ?array $message, array $includeSteps = [], ?string $flowSubject = null): void
-            {
-            }
-
-            public function openQueues(): int
-            {
-                return 0;
-            }
-
-            public function observeQueue(float $maxExecutionTimeInSeconds = 0.0): iterable
-            {
-                return [];
-            }
-
-            public function findAllFlowHashes(): iterable
-            {
-                return [];
-            }
-
-            public function findAllQueues(SortEnum $sortEnum = SortEnum::DESC): iterable
-            {
-                return [];
-            }
-
-            public function findAllSchemas(): iterable
-            {
-                return [];
-            }
-
-            public function findAllMessageSources(): iterable
-            {
-                return [];
-            }
-
-            public function findFlowInstanceByHash(string $flowHash): ?\Wundii\Flowcrafter\Storage\Entity\FlowInstanceEntity
-            {
-                return null;
-            }
-
-            public function findFlowByHash(string $flowHash): ?Flow
-            {
-                return null;
-            }
-
-            public function findFlowByRuntimeHash(string $flowRuntimeHash): ?Flow
-            {
-                return null;
-            }
-
-            public function findStepSourceByHash(string $stepHash): ?\Wundii\Flowcrafter\Storage\Entity\StepSourceEntity
-            {
-                return null;
-            }
-
-            public function findStepSourcesByStepSource(string $stepSource): iterable
-            {
-                return [];
-            }
-
-            public function findMessageSourceByMessageSource(string $messageSource): iterable
-            {
-                return [];
-            }
-        };
     }
 }
